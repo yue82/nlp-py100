78781bac538ac74d7bb9796e823fa214f07d5843
tktr <no.82.vuf.lab@gmail.com>
2015-11-12 16:20:12 +0900

    finish 30.py

--- 30.py
+++ 30.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 # coding:utf-8
 
+
 def read_mecab(infile):
-    test_count = 0
+
     full_seq = []
     with open(infile, 'r') as fi:
         seq = []
@@ -13,22 +14,29 @@ def read_mecab(infile):
                 if len(seq) != 0:
                     full_seq.append(seq)
                     seq = []
-                    if test_count == 2:
-                        break
-                    test_count += 1
                 continue
 
             surface, info = uni_line.split(u'\t')
-            base, pos, pos1 = info.split(u',')[:3]
-            org = info.split(u',')[-3]
-            morph = {(surface, base, pos, pos1):org}
+            pos, pos1 = info.split(u',')[:2]
+            base = info.split(u',')[-3]
+            morph = {
+                u'surface': surface,
+                u'base': base,
+                u'pos': pos,
+                u'pos1': pos1,
+            }
             seq.append(morph)
 
-    # print full_seq
+#     for s in full_seq:
+#         for ss in s:
+#             for k, v in ss.items():
+#                 print k, v,
+#             print ''
 
-def main():
-    read_mecab('neko.txt.mecab')
+
+# def main():
+#     read_mecab('neko.txt.mecab')
 
 
-if __name__ == '__main__':
-   main()
+# if __name__ == '__main__':
+#     main()
