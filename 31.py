c50494b6f7e176ec63f608ce7344ac4aea3f7909
tktr <no.82.vuf.lab@gmail.com>
2015-11-17 15:04:48 +0900

    add 31 & 32

---
+++ 31.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# coding:utf-8
+
+import MecabTools
+
+def main():
+    mr = MecabTools.MecabReader()
+    full_seq = mr.read_mecab('neko.txt.mecab')
+    for seq in full_seq:
+        for morph in seq:
+            if morph['pos'] == u'動詞':
+                print morph['surface']
+
+if __name__ == '__main__':
+   main()
